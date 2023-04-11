40ea9f760e7d223c1613cf1ca0ebd9ba41f05499
John <johnzhengyuliu@gmail.com>
2023-04-10 19:51:09 -0500

    test magic connect with web3modal

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,58 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
+import "@/styles/globals.css"
+
+import { EthereumClient, w3mConnectors, w3mProvider } from "@web3modal/ethereum"
+import { Web3Modal } from "@web3modal/react"
+import { configureChains, createClient, WagmiConfig } from "wagmi"
+import { sepolia, goerli } from "wagmi/chains"
+
+import { MagicConnectConnector } from "wagmi-magic-connect"
+
+const projectId = process.env.NEXT_PUBLIC_PROJECT_ID!
+const chains = [sepolia, goerli]
+const { provider } = configureChains(chains, [w3mProvider({ projectId })])
+
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: [
+    new MagicConnectConnector({
+      chains: chains,
+      options: {
+        apiKey: process.env.NEXT_PUBLIC_MAGICKEY!,
+        magicSdkConfiguration: {
+          network: {
+            rpcUrl: process.env.NEXT_PUBLIC_SEPOLIA_RPC!,
+            chainId: 11155111,
+          },
+        },
+      },
+    }),
+    ...w3mConnectors({ chains, version: 2, projectId }),
+  ],
+  provider,
+})
+
+const ethereumClient = new EthereumClient(wagmiClient, chains)
+
+const walletImages = {
+  magic: "https://svgshare.com/i/iJK.svg",
+}
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <ChakraProvider>
+        <WagmiConfig client={wagmiClient}>
+          <Component {...pageProps} />
+        </WagmiConfig>
+      </ChakraProvider>
+
+      <Web3Modal
+        projectId={projectId}
+        ethereumClient={ethereumClient}
+        walletImages={walletImages}
+      />
+    </>
+  )
 }
